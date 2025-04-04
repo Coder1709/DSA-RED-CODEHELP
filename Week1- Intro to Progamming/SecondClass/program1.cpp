de122f08c35a734e11167f2601270cd2512652ce
Arpit Pathak <coderinashell@gmail.com>
2025-04-04 20:58:28 +0530

    Second class

---
+++ Week1- Intro to Progamming/SecondClass/program1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout << "Hello, World!" << endl;
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+//HomeWork
+//alternate of endl 
+//read about ner line and flush
+
+//characters of 32 bit and 64bit
+//difference between 32 bit and 64 bit
+//diffwerence in storage of +ve and -ve integers
+
+//signed and unsigned variables
+
+
+//1s complement and 2s complement
+
+//how it gets converted
+
+
+//open object file and read it  
+//open executable file and read it
+
+//read about hight level language
+//high level language to low level language
+//high level language to machine code   
+//high level language to assembly language
+//high level language to object code
+//high level language to binary code
+//high level language to executable code
+//high level language to source code    
+
+
